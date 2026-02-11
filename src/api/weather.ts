79e30a1dc95588887a79c889da91c8f3f7cbb85d
houyhOvO <houyh081777@gmail.com>
2026-02-11 23:04:49 +0800

    feat: add api to get weather

---
+++ src/api/weather.ts
@@ -0,0 +1,28 @@
+import axios from "axios"
+
+const request = axios.create({
+  baseURL: "https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/"
+})
+
+request.interceptors.request.use(
+  (config) => {
+  const API_KEY = import.meta.env.VITE_API_KEY;
+  config.params = {...config.params, key: API_KEY};
+  return config;
+}, (error) => {
+    Promise.reject(error);
+})
+
+request.interceptors.response.use((response) =>{
+  return response.data;
+}, (error) => {console.log(error)})
+
+export const getWeather = (city: string) => {
+  return request({
+    method: "GET",
+    url: `${city}`,
+    params: {
+      unitGroup: 'metric'
+    }
+  })
+}
